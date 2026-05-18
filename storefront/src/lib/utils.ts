56cf32fc9880b1118a48eebd50a4b69196d6a9b7
sunil saini <sunilsbipo8209@gmail.com>
2026-05-18 13:58:06 +0530

    Fix storefront typography inheritance

--- storefront/src/lib/utils.ts
+++ storefront/src/lib/utils.ts
@@ -1,5 +1,24 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    theme: {
+      text: [
+        'display-xl',
+        'display-lg',
+        'display-md',
+        'display-sm',
+        'body-xl',
+        'body-lg',
+        'body-md',
+        'body-sm',
+        'body-xs',
+        'count-xs',
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
